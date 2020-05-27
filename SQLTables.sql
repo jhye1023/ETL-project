6f84038ac720f5011e360b5994aba7b4db4aafba
JIHYE CHOI <jihyechoi@jihyes-mbp.attlocal.net>
2020-05-26 20:35:11 -0700

    uploading my work

---
+++ SQLTables.sql
@@ -0,0 +1,25 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "gun_violance" (
+    "State" VARCHAR   NOT NULL,
+    "Children_Killed" integer   NOT NULL,
+    "Teen_Killed" integer   NOT NULL,
+    "Acci_Child_Killed" integer   NOT NULL,
+    "Acci_Teen_Killed" integer   NOT NULL,
+    "Total_Killed" integer   NOT NULL
+);
+
+CREATE TABLE "guntocarry" (
+    "State" VARCHAR   NOT NULL,
+    "Population_perc" VARCHAR   NOT NULL
+);
+
+CREATE TABLE "weapon_used" (
+    "State" VARCHAR   NOT NULL,
+    "Total_murders" VARCHAR   NOT NULL,
+    "Total_firearms" VARCHAR   NOT NULL,
+    "Total_Weapon_Used" VARCHAR   NOT NULL
+);
+
